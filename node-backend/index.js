39d0c604d9d072aa2480dffe98de2699e9cfcbc7
alxndr2000 <hissrunboom@gmail.com>
2024-01-05 12:20:04 +0000

    reformatted data output to make it possible to integrate custom availability feature

--- node-backend/index.js
+++ node-backend/index.js
@@ -4,62 +4,96 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const database_fake = {
-    rooms: {
-        rc01: {
-            roomname: "Bob's Room",
-            users: [
-                {
-                    name: "Bob",
-                    availability_days: [1,1,0,1,0,0,1] // monday to sunday as true/false
-                },
-                {
-                    name: "Alice",
-                    availability_days: [0,0,0,1,0,1,1]
-                },
-                {
-                    name: "Dave",
-                    availability_days: [1,1,1,0,0,0,1]
-                },
-                {
-                    name: "Clyde",
-                    availability_days: [0,1,0,1,0,1,1]
-                }
+    "rooms": {
+        "rc01": {
+            "roomname": "Meeting Room A",
+            "users": [
+                {"id": 0, "name": "Bob", "availability_days": [1,1,0,1,0,0,1]},
+                {"id": 1, "name": "Alice", "availability_days": [0,0,0,1,0,1,1]},
+                {"id": 2, "name": "Dave", "availability_days": [1,1,1,0,0,0,1]},
+                {"id": 3, "name": "Clyde", "availability_days": [0,1,0,1,0,1,1]}
             ]
         },
-        rc02: {
-            roomname: "Bob's Room",
-            users: [
-                {
-                    name: "Evan",
-                    availability_days: [0,1,1,1,1,0,1] // monday to sunday as true/false
-                },
-                {
-                    name: "Frank",
-                    availability_days: [1,0,1,0,0,1,1]
-                },
-                {
-                    name: "George",
-                    availability_days: [0,1,1,0,1,0,0]
-                },
-                {
-                    name: "Harry",
-                    availability_days: [0,1,1,0,0,1,1]
-                }
+        "rc02": {
+            "roomname": "Conference Room B",
+            "users": [
+                {"id": 0, "name": "Evan", "availability_days": [0,1,1,1,1,0,1]},
+                {"id": 1, "name": "Frank", "availability_days": [1,0,1,0,0,1,1]},
+                {"id": 2, "name": "George", "availability_days": [0,1,1,0,1,0,0]},
+                {"id": 3, "name": "Harry", "availability_days": [0,1,1,0,0,1,1]}
+            ]
+        },
+        "rc03": {
+            "roomname": "Planning Room C",
+            "users": [
+                {"id": 0, "name": "John", "availability_days": [1,0,1,0,1,1,0]},
+                {"id": 1, "name": "Mary", "availability_days": [0,1,1,1,0,0,1]},
+                {"id": 2, "name": "Tom", "availability_days": [0,1,0,1,1,0,1]}
+            ]
+        },
+        "rc04": {
+            "roomname": "Event Coordination Room",
+            "users": [
+                {"id": 0, "name": "Anna", "availability_days": [1,1,1,0,1,0,0]},
+                {"id": 1, "name": "Peter", "availability_days": [0,1,0,1,0,1,1]}
+            ]
+        },
+        "rc05": {
+            "roomname": "Team Collaboration Room",
+            "users": [
+                {"id": 0, "name": "Sophie", "availability_days": [1,0,0,1,1,0,1]},
+                {"id": 1, "name": "Jack", "availability_days": [0,1,0,1,1,0,1]},
+                {"id": 2, "name": "Emily", "availability_days": [1,1,0,0,1,0,1]},
+                {"id": 3, "name": "William", "availability_days": [0,0,1,1,0,1,1]}
+            ]
+        },
+        "rc06": {
+            "roomname": "Project Planning Room",
+            "users": [
+                {"id": 0, "name": "Michael", "availability_days": [0,1,1,0,1,1,0]},
+                {"id": 1, "name": "Emma", "availability_days": [1,0,0,1,0,1,1]}
+            ]
+        },
+        "rc07": {
+            "roomname": "Event Logistics Room",
+            "users": [
+                {"id": 0, "name": "Oliver", "availability_days": [0,1,1,1,0,0,1]},
+                {"id": 1, "name": "Ava", "availability_days": [1,0,1,0,1,0,0]}
+            ]
+        },
+        "rc08": {
+            "roomname": "Event Planning Hub",
+            "users": [
+                {"id": 0, "name": "Liam", "availability_days": [0,0,1,1,0,1,1]},
+                {"id": 1, "name": "Mia", "availability_days": [1,1,0,1,1,0,0]},
+                {"id": 2, "name": "Sophia", "availability_days": [1,0,1,0,1,1,0]},
+                {"id": 3, "name": "Jackson", "availability_days": [0,1,0,1,0,1,1]},
+                {"id": 4, "name": "Ella", "availability_days": [1,1,0,0,1,0,1]},
+                {"id": 5, "name": "Lucas", "availability_days": [0,0,1,1,0,1,1]},
+                {"id": 6, "name": "Aiden", "availability_days": [1,1,0,1,0,0,1]},
+                {"id": 7, "name": "Chloe", "availability_days": [0,1,1,0,1,0,0]},
+                {"id": 8, "name": "Lincoln", "availability_days": [1,0,0,1,1,1,0]}
             ]
         }
     }
-    
-
 }
 
-function calculateAvailableDays(roomData, roomCode){
-    generalAvailability=[[],[],[],[],[],[],[]]
+function generateAvailabilityJSON(roomData, roomCode){
+    var userIDs = []
+
+    var room = roomData.rooms[roomCode]
+
+    room.users.forEach(user => {
+        userIDs.push({'ID': user.id, 'name': user.name})
+    });
+
+    var generalAvailability=[[],[],[],[],[],[],[]]
     try {
         // Calculate number of people available on each day
-        roomData.rooms[roomCode].users.forEach(user => {
+        room.users.forEach(user => {
             for (let day = 0; day < 7; day++) {
                 if (user.availability_days[day]) {
-                    generalAvailability[day].push(user.name);
+                    generalAvailability[day].push(user.id);
                 }
                 
             }
@@ -72,7 +106,7 @@ function calculateAvailableDays(roomData, roomCode){
         }
     
     
-        return returnJson;
+        return {'users':userIDs, "data": returnJson};
     }
 
     
@@ -84,7 +118,7 @@ app.get("/api/*", (req, res) => { //rc01 is currently hardcoded into the react a
         wildcardPath="rc01"
     }
     console.log("Request for ", wildcardPath)
-    const availability = calculateAvailableDays(database_fake, wildcardPath);
+    const availability = generateAvailabilityJSON(database_fake, wildcardPath);
     res.json(availability);
 });
 
--- react-frontend/src/BoxComponent.jsx
+++ react-frontend/src/BoxComponent.jsx
@@ -5,45 +5,47 @@ import React, { useState, useEffect } from 'react';
 const BoxComponent = ({ boxId, allBoxData }) => {
   const [boxData, setBoxData] = useState(null);
 
-  function nameDataFormatter(namesArray, maxLength) {
-    if (boxData!=null) {
-      const totalCharacters = namesArray.reduce((sum, name) => sum + name.length, 0);
-
-      if (totalCharacters <= maxLength) {
-        return namesArray.join(', ');
-      } else {
-        let currentLength = 0;
-        let truncatedNames = [];
-
-        for (const name of namesArray) {
-          if (currentLength + name.length <= maxLength) {
-            truncatedNames.push(name);
-            currentLength += name.length + 2; // Adding 2 for comma and space
-          } else {
-            break;
-          }
+  function nameDataFormatter(usersArray, selectedIds, maxLength) {
+    // Check if the necessary data is available
+    if (!usersArray || !selectedIds) {
+      return "Data not available";
+    }
+  
+    const selectedNames = usersArray
+      .filter(user => selectedIds.includes(user.ID))
+      .map(user => user.name);
+  
+    const totalCharacters = selectedNames.reduce((sum, name) => sum + name.length, 0);
+  
+    if (totalCharacters <= maxLength) {
+      return selectedNames.join(', ');
+    } else {
+      let currentLength = 0;
+      let truncatedNames = [];
+  
+      for (const name of selectedNames) {
+        if (currentLength + name.length <= maxLength) {
+          truncatedNames.push(name);
+          currentLength += name.length + 2; // Adding 2 for comma and space
+        } else {
+          break;
         }
-
-        const remainingCount = namesArray.length - truncatedNames.length;
-        return `${truncatedNames.join(', ')} +${remainingCount} more`;
       }
-    }
-
-  }
-  function lengthCalculator(boxData) {
-
-    if (boxData!=null) {
-      return boxData.length;
-    }
-    else {
-      return 0
+  
+      const remainingCount = selectedNames.length - truncatedNames.length;
+      return `${truncatedNames.join(', ')} +${remainingCount} more`;
     }
   }
 
 
   useEffect(() => {
     // Set boxData based on the provided allBoxData
-    setBoxData(allBoxData[`box${boxId}`]); // Assuming boxId starts from 0
+    
+      // Ensure allBoxData is defined and has the expected structure
+    if (allBoxData && allBoxData.data) {
+      // Set boxData based on the provided allBoxData
+      setBoxData({'users': allBoxData.users, 'data': allBoxData.data[`box${boxId}`]});
+    }
   }, [boxId, allBoxData]);
 
 
@@ -51,8 +53,9 @@ const BoxComponent = ({ boxId, allBoxData }) => {
   return <div className="box">
         <div className="top-left-number">{boxId+1}</div>
         <div className="box-data">
-        <p className='amount-data'>{lengthCalculator(boxData)} Available </p>
-        <p className='name-data'>{nameDataFormatter(boxData, 16)}</p>
+        <p className='amount-data'>{boxData ? `${boxData.data.length} Available` : 'Loading...'}</p>
+        <p className='name-data'>{boxData ? nameDataFormatter(boxData.users, boxData.data, 16) : 'Loading...'}</p>
+        
         </div>
     </div>;
 };
